ef8204a8eeff8002cc79f515cca40dbc9f16fff9
valdairelaborata <valdairelaborata@gmail.com>
2023-03-14 21:05:58 -0300

    start-02

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,16 @@
 
   <ul>
     <li v-for="item in itens" :key="item.id">
-      <p>
+      <p v-if="!item.editando">
         {{ item.id }} - {{ item.nome }}
-        <button v-if="item.finalizado">Excluir</button>
-        <button v-else>Editar</button>
+        <button v-if="item.finalizado" @click="excluir(item)">Excluir</button>
+        <button v-else @click="alterar(item)">Editar</button>
       </p>
+      <div v-else>
+        {{ item.nome }}
+        <input v-model="item.nome">
+        <button @click="confirmar(item)">Confirmar</button>
+      </div>
     </li>
   </ul>
 </template>
@@ -19,17 +24,40 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 
+
 export default {
 
   data() {
     return {
       titulo: "Atividades",
       itens: [
-        { id: 1, nome: 'Item 001', finalizado: false },
-        { id: 2, nome: 'Item 002', finalizado: true },
-        { id: 3, nome: 'Item 003', finalizado: true },
-        { id: 4, nome: 'Item 004', finalizado: false }
+        { id: 1, nome: 'Item 001', finalizado: false, editando: false },
+        { id: 2, nome: 'Item 002', finalizado: true, editando: false },
+        { id: 3, nome: 'Item 003', finalizado: true, editando: false },
+        { id: 4, nome: 'Item 004', finalizado: false, editando: false }
       ]
+    }
+  },
+  methods: {
+    excluir(item) {
+
+      if (confirm('Deseja excluir o item ' + item.nome + '?')) {
+        this.itens = this.itens.filter(x => x.id != item.id)
+      }
+
+    },
+    alterar(item) {
+      let registro = this.itens.find((x) => {
+        return x.id === item.id
+      })
+
+      registro.editando = true
+    },
+    confirmar(item) {
+      let registro = this.itens.find((x) => {
+        return x.id === item.id
+      })
+      registro.editando = false
     }
   }
 }
